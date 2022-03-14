0fbc5ed06d21b6e979865f37fa3265290ad55b2f
hazedrops <hazedrops@hotmail.com>
2022-03-13 22:32:14 -0700

    Container, Grid, AppBar, MenuIcon and theme customization, etc

--- src/App.js
+++ src/App.js
@@ -8,61 +8,133 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import Checkbox from '@mui/material/Checkbox'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import TextField from '@mui/material/TextField'
+// import { makeStyles } from '@mui/styles/makeStyles'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { green, orange } from '@mui/material/colors'
+import 'fontsource-roboto'
+import  { Typography } from '@mui/material'
+import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+
+
+
+const theme = createTheme({
+  typography: {
+    h2: {
+      fontSize: 50,
+      marginBottom: 15,
+    }
+  },
+  palette: {
+    primary: {
+      main: green[500],
+    },
+    secondary: {
+      main: orange[800],
+    },
+  },
+})
+
+// const useStyles = makeStyles(() => ({
+//   background: 'linear-gradient(45deg, #333, #999)',
+//   border: 0,
+//   borderRadius: 15,
+//   color: 'white',
+//   padding: '0 30px',
+// }))
+
+// function ButtonStyled() {
+//   // const classes = useStyles()
+
+//   return <Button className={classes.root}>Test Styled Button</Button>
+// }
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true)
 
   return (
-    <FormControlLabel 
+    <FormControlLabel
       control={
-        <Checkbox 
+        <Checkbox
           checked={checked}
           icon={<DeleteIcon />}
           checkedIcon={<SaveIcon />}
           onChange={(e) => setChecked(e.target.checked)}
           inputProps={{
-            'aria-label': 'secondary checkbox'
+            'aria-label': 'secondary checkbox',
           }}
-        />}
-        label="Testing Checkbox"
+        />
+      }
+      label='Testing Checkbox'
     />
   )
 }
 
-
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <TextField 
-          variant='filled'
-          color='secondary'
-          type='email'
-          placeholder='test@test.com'
-        />
-        <CheckboxExample />
-        <ButtonGroup>
-          <Button
-            startIcon={<SaveIcon />}
-            size='large'
-            variant='contained'
-            color='primary'
-          >
-            Save
-          </Button>
-          <Button
-            startIcon={<DeleteIcon />}
-            size='large'
-            variant='contained'
-            color='secondary'
-          >
-            Discard
-          </Button>
-        </ButtonGroup>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='xl'>
+        <div className='App'>
+          <header className='App-header'>
+            <AppBar color='secondary'>
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variation='h6'>MUI Themeing</Typography>
+                <Button style={{color: 'black'}}>Login</Button>
+              </Toolbar>
+            </AppBar>
+            <Typography variant='h2' component='div'>
+              Welcome to MUI
+            </Typography>
+            <Typography variant='subtitle1'>
+              Learn how to use Material UI
+            </Typography>
+            {/* <ButtonStyled /> */}
 
-        <img src={logo} className='App-logo' alt='logo' />
-      </header>
-    </div>
+            <Grid container spacing={2} justifyContent='center'>
+              <Grid item xs={12} md={6}>
+                <Paper style={{ height: 75, width: '100%' }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 75, width: 50 }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 75, width: 50 }} />
+              </Grid>
+            </Grid>
+
+            <CheckboxExample />
+            <ButtonGroup>
+              <Button
+                startIcon={<SaveIcon />}
+                size='large'
+                variant='contained'
+                color='primary'
+              >
+                Save
+              </Button>
+              <Button
+                startIcon={<DeleteIcon />}
+                size='large'
+                variant='contained'
+                color='secondary'
+              >
+                Discard
+              </Button>
+            </ButtonGroup>
+
+            <img src={logo} className='App-logo' alt='logo' />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   )
 }
 
